785985f99d5f794a4013f9fd28ca76bdd42792d3
luozhonghua <172257861@qq.com>
2018-09-18 10:50:24 +0800

    add

--- cjs-oauth2-sso-auth-server/src/main/resources/sql.sql
+++ cjs-oauth2-sso-auth-server/src/main/resources/sql.sql
@@ -1,15 +1,15 @@
 CREATE TABLE oauth_client_details (
-    client_id VARCHAR(128) PRIMARY KEY,
-    resource_ids VARCHAR(256),
-    client_secret VARCHAR(256),
-    scope VARCHAR(256),
-    authorized_grant_types VARCHAR(256),
-    web_server_redirect_uri VARCHAR(256),
-    authorities VARCHAR(256),
+    client_id VARCHAR(128) PRIMARY KEY  COMMENT '客户端id',
+    resource_ids VARCHAR(256) COMMENT '资源id',
+    client_secret VARCHAR(256) COMMENT '客户端公/私密钥',
+    scope VARCHAR(256) COMMENT '授权范围',
+    authorized_grant_types VARCHAR(256)  COMMENT '授权类型',
+    web_server_redirect_uri VARCHAR(256) COMMENT '重定向URI',
+    authorities VARCHAR(256)  COMMENT '授权者',
     access_token_validity INTEGER,
     refresh_token_validity INTEGER,
     additional_information VARCHAR(256),
-    autoapprove VARCHAR(256)
+    autoapprove VARCHAR(256) COMMENT '自动授权批准人'
 );
 INSERT INTO oauth_client_details (client_id, client_secret, scope, authorized_grant_types, web_server_redirect_uri, autoapprove)
 VALUES ('MemberSystem', '$2a$10$dYRcFip80f0jIKGzRGulFelK12036xWQKgajanfxT65QB4htsEXNK', 'user_info', 'authorization_code', 'http://localhost:8081/login', 'user_info');
